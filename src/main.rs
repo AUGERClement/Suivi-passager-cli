a558b893b75b61a4ff2b39e9c624c8d8c2d9a24e
AUGERClement <clement.auger@epitech.eu>
2022-06-17 18:13:13 +0200

    defined structures and enum, along with their duisplay protocols

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,54 @@
 use std::env;
+use std::fmt; // For formatting structs print
+
+// Define a snapshot for the current bus situation
+// It can then be logged
+struct BusSnapshot {
+    stop_name: String, // The current stop of the bus
+    incoming: i32, // The number of entering passengers in this stop
+    outgoing: i32, // The number of outgoing passengers in this stop
+    current: i32, // The current number of passengers in the bus
+}
+
+impl fmt::Display for BusSnapshot {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{},{},{},{}", self.stop_name, self.incoming, self.outgoing, self.current)
+    }
+}
+
+// The type of change in passenger the get_input must ask
+enum TypeUpdate {
+    Incoming,
+    Outgoing,
+}
+
+impl fmt::Display for TypeUpdate {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+       match *self {
+           TypeUpdate::Incoming => write!(f, "incoming"),
+           TypeUpdate::Outgoing => write!(f, "outgoing"),
+       }
+    }
+}
+
+
+fn get_input(stop: &str, status: TypeUpdate) -> i32 {
+    println!("[{}] Input number of {} passagers :", stop, status);
+    0
+}
 
 // Update the state of the bus for the stop, and log it.
-fn update_bus(stop: &str) -> () {
-    println!("[{}] Number of incoming passagers :", stop);
-    println!("[{}] Number of outgoing passagers :", stop);
+// Takes a stop name and the population from the last stop.
+fn update_bus(stop: &str, population:i32) -> () {
+    let bus_snapshot = BusSnapshot {
+        stop_name: String::from(stop),
+        incoming: get_input(stop, TypeUpdate::Incoming),
+        outgoing: get_input(stop, TypeUpdate::Outgoing),
+        current: population,//+ incoming - outgoing,
+    };
+    //println!("[{}] Number of incoming passagers : {}", stop, bus_snapshot.incoming);
+    //println!("[{}] Number of outgoing passagers : {}", stop, bus_snapshot.outgoing);
+    println!("Snapshot : {}", bus_snapshot);
     // Prompt "[stop] Number of ingoing passagers ?"
     // Prompt "[stop] Number of outgoing passagers ?"
     // Log status
@@ -18,6 +63,6 @@ fn main() {
     let stops = stop.split(',');
     let trimmed = stops.map(|stop| stop.trim());
     for stop in trimmed {// Yeah map doesn't behave well with print
-        update_bus(stop);
+        update_bus(stop, 0);
     }
 }
