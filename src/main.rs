11cc13a6e3509b1a7945867dbccfab1a2cdae709
AUGERClement <clement.auger@epitech.eu>
2022-06-19 18:55:12 +0200

    added error handling for passengers input

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::env; // To obtain config
 use std::fmt; // For formatting structs print
 use std::io::stdin; // To get user input
+use std::fs::OpenOptions; // To append in file
+use std::io::Write; // To write in file
 
 // Define a snapshot for the current bus situation
 // It can then be logged
@@ -35,33 +37,54 @@ impl fmt::Display for TypeUpdate {
 }
 
 // Get a user input for number of passengers stepping in our out.
-// In case of failure, the update bus will reiterate whole registration for the stop.
+// Accept only valid integer and will prompt until it get one.
 fn get_input(stop: &str, status: TypeUpdate) -> i32 {
     let mut input = String::new();
-    //let mut input = String::from("3");
 
     println!("[{}] Please input number of {} passagers : ", stop, status);
-    stdin().read_line(&mut input).expect("error: unable to read user input");
-    println!("input is {}", input);
-    let passengers = match input.parse::<i32>() {
-        Ok(val) if val >= 0 => val,
-        _ => panic!("error : expected positive integer"), // Cover err and negative vals
+    let passengers: i32 = loop {
+        stdin().read_line(&mut input).expect("error: unable to read user input");
+        if let Ok(val) = input.trim().parse::<i32>() { // Success read
+            if val >= 0 {
+                break val;
+            }
+        }
+        println!("error : expected positive integer");
+        input.clear(); // Clear the buffer
     };
     println!("[{}] Registering {} {} passagers", stop, passengers, status);
     passengers
 }
 
+// Log the update in statement.txt.
+// No computation, side_effect only.
+// Src : https://stackoverflow.com/questions/30684624/what-is-the-best-variant-for-appending-a-new-line-in-a-text-file
+fn log_update(bus_snapshot: &BusSnapshot) {
+    let mut file = OpenOptions::new()
+        .write(true)
+        .append(true)
+        .open("statement.txt")
+        .unwrap();
+
+    if let Err(e) = writeln!(file, "{}", bus_snapshot) {
+        eprintln!("Couldn't write to file: {}", e);
+    };
+    println!("Snapshot : {}", bus_snapshot);
+}
+
 // Update the state of the bus for the stop, and log it.
 // Takes a stop name and the population from the last stop.
-fn update_bus(stop: &str, population:i32) -> () {
-    let bus_snapshot = BusSnapshot {
+// Return the current population (used as arg for the next call)
+fn update_bus(stop: &str, population:i32) -> i32 {
+    let mut bus_snapshot = BusSnapshot { // Mutable because I need filed to compute population
         stop_name: String::from(stop),
         incoming: get_input(stop, TypeUpdate::Incoming),
         outgoing: get_input(stop, TypeUpdate::Outgoing),
         current: population,//+ incoming - outgoing,
     };
-    println!("Snapshot : {}", bus_snapshot);
-    // Append in file
+    // compute and ensure current population is valid
+    //log_update(&bus_snapshot); // Append in file (not yet setup)
+    bus_snapshot.current
 }
 
 fn main() {
@@ -71,7 +94,13 @@ fn main() {
     };
     let stops = stop.split(',');
     let trimmed = stops.map(|stop| stop.trim());
-    for stop in trimmed {// Yeah map doesn't behave well with print
-        update_bus(stop, 0);
+    let mut population:i32 = 0;
+    
+    // Init statement.txt
+    for stop in trimmed {// A fold wouldn't be as readable for non FP cultists.
+        population = update_bus(stop, population);
     }
+    // Potential fold in pseudo_code :
+    // fold(acc, list, fn);
+    // fold(population, trimmed, update_bus(&1, acc))
 }
